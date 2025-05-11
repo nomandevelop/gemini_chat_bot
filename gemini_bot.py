4b8b9512063ff80896b95b843d38dacbbed4a35c
Noman <154869835+nomandevelop@users.noreply.github.com>
2025-05-11 14:33:39 +0500

    gemini_bot.py

--- gemini_bot.py
+++ gemini_bot.py
@@ -4,7 +4,8 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv("fun_load2.env") 
+# load_dotenv("fun_load2.env") 
+API_KEY = st.secrets["OPENROUTER_API_KEY"]
 
 # --- Page Configuration (must be the first Streamlit command) ---
 st.set_page_config(
